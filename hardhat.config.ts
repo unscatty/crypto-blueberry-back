ec66b5d827efd3d4e713087d4b28e06e1756cba2
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-10-03 21:10:43 -0500

    Auto fetch RSK gas price

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,17 +5,33 @@ import '@typechain/hardhat'
 import '@nomiclabs/hardhat-ethers'
 // import '@nomiclabs/hardhat-waffle'
 
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+import { config as dotenvconfig } from 'dotenv'
+import syncFetch from 'sync-fetch'
+
+dotenvconfig();
+
+const mnemonic = process.env.MNEMONIC;
 
 if (!mnemonic || mnemonic.split(' ').length !== 12) {
   throw new Error('unable to retrieve mnemonic from .secret');
 }
 
-const gasPriceTestnetRaw = fs.readFileSync(".gas-price-testnet.json").toString().trim();
-const gasPriceTestnet = parseInt(JSON.parse(gasPriceTestnetRaw).result, 16);
+const gasPriceTestnetRaw = syncFetch('https://public-node.testnet.rsk.co/', {
+  method: 'POST',
+  body: JSON.stringify({
+    jsonrpc: '2.0', 
+    method: 'eth_gasPrice', 
+    params: [], 
+    id: 1
+  }),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}).json();
+
+const gasPriceTestnet = parseInt(gasPriceTestnetRaw.result, 16);
 if (typeof gasPriceTestnet !== 'number' || isNaN(gasPriceTestnet)) {
-  throw new Error('unable to retrieve network gas price from .gas-price-testnet.json');
+  throw new Error('unable to retrieve network gas price from "https://public-node.testnet.rsk.co/"');
 }
 console.log("Gas price Testnet: " + gasPriceTestnet);
 
@@ -34,7 +50,11 @@ const config: HardhatUserConfig = {
       gasPrice: Math.floor(gasPriceTestnet * 1.1),
       gasMultiplier: 1.1,
       timeout: 1e9
-    }
+    },
+    development: {
+      url: '127.0.0.1:8545',
+      chainId: 5777
+    },
   }
 };
 
