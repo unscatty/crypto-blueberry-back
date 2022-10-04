3cc95b27ad189832af2975bd92be0036038f21ed
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-10-04 09:24:29 -0500

    Modify Txs names

--- test/berry-test.ts
+++ test/berry-test.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
 
-describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
+describe("Berry contract", function () {
+  it("Show group creation and plan payment", async function () {
     const BerryContract = await ethers.getContractFactory('Berry');
     const berry = await BerryContract.deploy();
 
@@ -15,13 +15,13 @@ describe("Token contract", function () {
     console.log(`User1 address: ${user1.address}`)
     console.log(`User2 address: ${user2.address}`)
 
-    const netflixProviderID = await berry.connect(owner).createProvider('nextflix', '', netflix.address);
+    const createProviderTx = await berry.connect(owner).createProvider('nextflix', '', netflix.address);
     const numProviders = await berry.numProviders();
 
-    const createdProvider = await berry.providers(netflixProviderID.value);
+    const createdProvider = await berry.providers(0);
 
     console.log(`Number od providers: ${numProviders.toNumber()}`)
-    console.log(`Netflix Provider: id: ${netflixProviderID.value.toNumber()}, address: ${netflix.address}`)
+    console.log(`Netflix Provider: id: ${0}, address: ${netflix.address}`)
     console.log(`Created provider: name: ${createdProvider.name}, address: ${createdProvider.serviceOwner}`)
 
     console.log(`Balance of user1 before joining group: ${(await user1.getBalance()).toString()}`);
@@ -33,10 +33,10 @@ describe("Token contract", function () {
     // ethers.utils.parseEther
 
     // const netflixMonthPlan = await berry.addPlan(berry.numProviders(), 'monthly netflix', 'pay netflix every month', 30, 6, 2);
-    await berry.connect(netflix).addPlan(netflixProviderID.value, 'monthly netflix 2', 'pay netflix every month', 30, 15, 5);
+    await berry.connect(netflix).addPlan(createProviderTx.value, 'monthly netflix 2', 'pay netflix every month', 30, 15, 5);
 
-    // const netflixMonthPlanID = await berry.connect(netflix).addPlan(netflixProviderID.value, 'monthly netflix', 'pay netflix every month', 30, ethers.utils.parseEther('10'), 2);
-    const netflixMonthPlanID = await berry.connect(netflix).addPlan(netflixProviderID.value, 'monthly netflix', 'pay netflix every month', 30, ethers.utils.parseEther('10.0'), 2);
+    // const netflixMonthPlanID = await berry.connect(netflix).addPlan(createProviderTx.value, 'monthly netflix', 'pay netflix every month', 30, ethers.utils.parseEther('10'), 2);
+    const netflixMonthPlanTx = await berry.connect(netflix).addPlan(0, 'monthly netflix', 'pay netflix every month', 30, ethers.utils.parseEther('10.0'), 2);
 
 
     // console.log(`Price per member: ${netflixMonthPlanValues.pricePerMember.toString()}`)
@@ -54,15 +54,15 @@ describe("Token contract", function () {
     console.log(`Balance of netflix after creating group: ${(await netflix.getBalance()).toString()}`);
 
     console.log(`Members in this group: ${group.numMembers.toString()}; Max members for this plan: ${plan.maxMembers.toString()}, price per member : ${plan.pricePerMember.toString()}`)
-    const joinedToGroup = await berry.connect(user2).joinGroup(0, 0, { value: ethers.utils.parseEther('15.0') });
+    const joinedToGroup = await berry.connect(user2).joinGroup(0, { value: ethers.utils.parseEther('15.0') });
     // const joinedToGroup = await berry.connect(user2).joinGroup(0, { value: ethers.utils.parseEther('5.0') });
 
     // const withdrawn = await berry.connect(netflix).withdrawFromGroup(0, 0);
 
     try {
-      expect(await berry.connect(user2).joinGroup(0, 0, { value: ethers.utils.parseEther('15.0') })).to.throw()
+      expect(await berry.connect(user3).joinGroup(0, { value: ethers.utils.parseEther('15.0') })).to.throw()
     } catch(e: any) {
-      console.log(e.message);
+      console.error(`Error joining group: ${e.message}`);
     }
 
     console.log(`Balance of user1 after joining group: ${(await user1.getBalance()).toString()}`);
